afb1c51e24a2a9a7ef0483f61c9c7c6d77fe022a
desgefhs <jhmin9414@gmail.com>
2026-02-07 18:45:48 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 18244 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,57 +4,45 @@ import java.util.Queue;
 
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static  boolean [] visited;
     static int [] arr;
     static int n;
     static Queue<Integer> queue;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.valueOf(br.readLine());
+        arr = new int[1000001];
+        queue = new LinkedList<>();
+
+        bfs(n);
+
+    }
     static void bfs(int start) {
 
         queue.offer(start);
-        visited[start] = true;
 
         while (!queue.isEmpty()) {
             int current = queue.poll();
 
+            if (current == 1) {
+                System.out.println(arr[current]);
+                return;
+            }
+
             for (int i = 3; i > 0; i--) {
                 int next = 0;
+
                 if (i == 1) next = current - 1;
                 else if (current % i == 0) {
                     next = current / i;
                 }
 
-                if (next == 1) {
-                    System.out.println(arr[current] + 1);
-                    return;
-                }
-
-                if (next > 1 && next < arr.length && arr[next] == 0) {
+                if (next >= 1 && next < arr.length && arr[next] == 0) {
                     arr[next] = arr[current] + 1;
                     queue.offer(next);
                 }
             }
         }
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
-
-        n = Integer.valueOf(br.readLine());
-        visited = new boolean[1000001];
-        arr = new int[1000001];
-        queue = new LinkedList<>();
-
-        if (n == 1) {
-            System.out.println("0");
-            return;
-        }
-
-        bfs(n);
-
     }
 
 }
